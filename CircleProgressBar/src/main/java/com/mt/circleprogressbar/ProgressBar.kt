e0ab678a311e3cba3fe1903e6d4011905dc57142
toneang <toneanggg@gmail.com>
2022-08-26 01:33:08 +0700

    mo.ton scale mode

--- CircleProgressBar/src/main/java/com/mt/circleprogressbar/ProgressBar.kt
+++ CircleProgressBar/src/main/java/com/mt/circleprogressbar/ProgressBar.kt
@@ -7,23 +7,36 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import kotlin.random.Random
 
 /**
  * TODO: document your custom view class.
  */
-class Progressbar : View , ValueAnimator.AnimatorUpdateListener{
+class Progressbar : View, ValueAnimator.AnimatorUpdateListener {
 
+    private lateinit var colorList: IntArray
     private var borderProgressBarSize: Float = 0f
     private var scaleColor: Int? = null
     private var valueColor: Int? = null
     private var valueProgressBar = 0f
+    private var scaleMode = ModeCirCle
+    private var mixValueColor:Int = 0
 
     private lateinit var paintScale: Paint
     private lateinit var paintValue: Paint
+
+    companion object {
+        const val BOX_STROKE_WIDTH = 5.0f
+        const val WidthFaceFrame = 34
+        const val HeightFaceFrame = 22
+        const val RadiusX = 34
+        const val RadiusY = 22
+        const val ModeCirCle = 0
+        const val ModeOval = 1
+    }
 
     constructor(context: Context) : super(context) {
         init(null, 0)
@@ -41,7 +54,7 @@ class Progressbar : View , ValueAnimator.AnimatorUpdateListener{
         init(attrs, defStyle)
     }
 
-    @SuppressLint("Recycle")
+    @SuppressLint("Recycle", "CustomViewStyleable")
     private fun init(attrs: AttributeSet?, defStyle: Int) {
         // Load attributes
         val a = context.obtainStyledAttributes(
@@ -54,7 +67,14 @@ class Progressbar : View , ValueAnimator.AnimatorUpdateListener{
         scaleColor = a.getColor(R.styleable.ProgressBar_scaleColor, Color.GREEN)
         valueColor = a.getColor(R.styleable.ProgressBar_valueColor, Color.RED)
         valueProgressBar =
-            IntToRadiusValue(a.getInt(R.styleable.ProgressBar_valueProgressBar, 25))
+            intToRadiusValue(a.getInt(R.styleable.ProgressBar_valueProgressBar, 25))
+
+        scaleMode = a.getInt(R.styleable.ProgressBar_modeProgressBar,1)
+        mixValueColor = a.getResourceId(R.styleable.ProgressBar_mixColor,R.array.progressBarMixColor)
+
+        if (mixValueColor != 0){
+            colorList = resources.getIntArray(mixValueColor)
+        }
 
         paintScale = Paint()
         paintScale.color = scaleColor as Int
@@ -75,26 +95,49 @@ class Progressbar : View , ValueAnimator.AnimatorUpdateListener{
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        if (scaleMode == ModeCirCle){
+            val radius = (this.width / 2) - 15F
+            canvas.drawCircle(
+                (this.width / 2).toFloat(),
+                (this.height / 2).toFloat(), radius, paintScale
+            )
+            canvas.drawArc(
+                RectF(
+                    this.width.toFloat() / 2 - radius,
+                    this.height.toFloat() / 2 - radius,
+                    this.width.toFloat() / 2 + radius,
+                    this.height.toFloat() / 2 + radius
+                ), -90F, valueProgressBar, false, paintValue
+            )
+        }else{
+            canvas.drawArc(
+                getRectFOval(), 0f, 360f, false, paintScale
+            )
 
-        val radius = (this.width / 2) - 15F
-        canvas.drawCircle(
-            (this.width / 2).toFloat(),
-            (this.height / 2).toFloat(), radius, paintScale
-        )
-
-        canvas.drawArc(
-            RectF(
-                this.width.toFloat() / 2 - radius,
-                this.height.toFloat() / 2 - radius,
-                this.width.toFloat() / 2 + radius,
-                this.height.toFloat() / 2 + radius
-            ), -90F, valueProgressBar, false, paintValue
-        )
-
-
+            canvas.drawArc(
+                getRectFOval(), valueProgressBar - 90f, valueProgressBar, false, paintValue
+            )
+        }
     }
 
-    private fun IntToRadiusValue(progressValue: Int): Float {
+    private fun getRectFOval(): RectF {
+        return RectF(
+            width.div(2).toFloat() - percentageWidth(width = width.toFloat()),
+            height.div(2).toFloat() - percentageHeight(height = height.toFloat()),
+            width.div(2).toFloat() + percentageWidth(width = width.toFloat()),
+            height.div(2).toFloat() + percentageHeight(height = height.toFloat())
+        )
+    }
+
+    private fun percentageWidth(width: Float): Float {
+        return (width * WidthFaceFrame) / 100
+    }
+
+    private fun percentageHeight(height: Float): Float {
+        return (height * HeightFaceFrame) / 100
+    }
+
+    private fun intToRadiusValue(progressValue: Int): Float {
         var value = progressValue
         if (progressValue > 100) {
             value = 100
@@ -103,18 +146,19 @@ class Progressbar : View , ValueAnimator.AnimatorUpdateListener{
 
     }
 
-    fun SetProgressBarValue(updateValue: Int) {
-
-        valueProgressBar = IntToRadiusValue(updateValue)
-        this.invalidate();
-
+    private fun setProgressBarValue(updateValue: Int) {
+        valueProgressBar = intToRadiusValue(updateValue)
+        if (updateValue == 100){
+            if (colorList.isNotEmpty()){
+                paintValue.color = colorList.random()
+            }
+        }
+        this.invalidate()
     }
 
     override fun onAnimationUpdate(animation: ValueAnimator?) {
-
         animation?.let {
-            SetProgressBarValue(it.animatedValue as Int)
+            setProgressBarValue(it.animatedValue as Int)
         }
-
     }
 }
--- app/src/main/java/com/mt/mycircleprogressbar/MainActivity.kt
+++ app/src/main/java/com/mt/mycircleprogressbar/MainActivity.kt
@@ -8,19 +8,20 @@ import com.mt.mycircleprogressbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainBinding:ActivityMainBinding
-    lateinit var valueAnimator:ValueAnimator
+    lateinit var mainBinding: ActivityMainBinding
+    lateinit var valueAnimator: ValueAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        valueAnimator = ValueAnimator.ofInt(0,65)
+        valueAnimator = ValueAnimator.ofInt(0, 100)
         valueAnimator.apply {
+            repeatCount = -1
             duration = 2000
             addUpdateListener(mainBinding.progressbar)
         }.start()
-
     }
+
 }
